4e85ec2a62c48e22765e70022e7469da98a3e355
carverh <pcarverb@hotmail.com>
2016-06-21 16:49:03 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+  include 'etc/config.php';
+  include 'bin/core.php';
+?>
+
+<html>
+  <head>
+    <title>Waddle</title>
+  </head>
+  <body>
+    <header>
+      <?php echo displayHeader() ?>
+    </header>
+    
+    <nav>
+      <ul>
+        <?php echo displayNav() ?>
+      </ul>
+    </nav>
+    
+    <main>
+      <?php echo displayMain() ?>
+    </main>
+    
+    <footer>
+      <?php echo displayFooter() ?>
+    </footer>
+  </body>
+</html>
