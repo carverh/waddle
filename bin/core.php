c3ec7771fbd7fc48f3a56a8523ed7cfabf229d16
carverh <pcarverb@hotmail.com>
2016-06-21 16:58:00 -0700

    Update core.php

--- bin/core.php
+++ bin/core.php
@@ -1,5 +1,24 @@
 <?php
   include '../etc/config.php';
+  include '../etc/nav.php';
   
-  function displayHeader() 
+  function displayHeader() {
+   echo '<h1 id="blog-title">';
+   echo $config['title'];
+   echo '</h1>'
+  }
+  
+  function displayNav() {
+   echo '<ul id="blog-nav">';
+   foreach $nav as $item {
+     echo '<li><a href="' . l"'
+   }
+   echo '</h1>'
+  }
+  
+  function displayNav() {
+   echo '<ul id="blog-nav">';
+   echo $config['title'];
+   echo '</h1>'
+  }
 ?>
