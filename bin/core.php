767f5e8c3db8b61ee9fc40244018ba6997325874
carverh <pcarverb@hotmail.com>
2016-06-21 17:45:30 -0700

    Update core.php

--- bin/core.php
+++ bin/core.php
@@ -1,5 +1,5 @@
 <?php
-  include '../etc/config.php' or die("[FATAL] Config Load Error");
+  include '../etc/config.php' or die('[FATAL] Config Load Error');
   
   function displayHeader() {
     echo '<h1 id="title">';
@@ -8,23 +8,29 @@
   }
   
   function displayNav() {
-    $nav = simplexml_load_file('../etc/nav.xml') or die("[FATAL] Nav File Load Error");
+    $nav = simplexml_load_file('../etc/nav.xml') or die('[FATAL] Nav File Load Error');
     echo '<ul id="nav">';
     foreach $nav['nav'] as $item {
       echo '<li class="navitem"><a class="navitem-link" href="' . $item['url'] . '">' . $item['text'] . '</a></li>';
     }
-    echo '</ul>'
+    echo '</ul>';
   }
   
   function displayMain() {
     foreach(scandir('../posts') as $post) {
-      $file_text = file_get_contents('../posts/' . $post)
+      $file = simplexml_load_file('../posts/' . $post) or die('[FATAL] Post Load Error');
+      $file_text = $file['text'];
+      $file_title = $file['title'];
       echo '<article>';
+      echo '<h1 class="post-title">';
+      echo '<a class="post-title-link" href="view.php/' . explode($post, '.xml')[0] . '">' . $file_title . '</a>';
+      echo '</h1>';
       if (strlen($file_text) > $config['home_post_length']) {
         ftruncate($file_text, $config['home_post_length']);
+        echo $file_text;
+        echo "..."
       }
-      echo $config['title'];
-      echo '</article>'
+      echo '</article>';
     }
   }
 ?>
