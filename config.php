6ad96e1405e82a8e3d317f60263074de0c7cb03b
carverh <pcarverb@hotmail.com>
2016-06-21 18:59:30 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,6 @@
 <?php
-  function load_config() {
-    $config['title'] = 'Test Blog';
-    $config['home_post_length'] = 512;
-    $config['author'] = 'Carver Harrison';
-    return $config;
-  }
+  $config['title'] = 'Test Blog';
+  $config['home_post_length'] = 512;
+  $config['author'] = 'Carver Harrison';
+  return $config;
 ?>
