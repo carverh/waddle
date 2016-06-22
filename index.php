49bf47ca3d64c2fbc48374ff19f50087c81f6694
carverh <pcarverb@hotmail.com>
2016-06-21 20:15:24 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 <html>
   <head>
     <title><?php displayTitle() ?></title>
-    <link rel="stylesheet" type="text/css" href="stylez.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
   </head>
@@ -19,10 +18,10 @@
         </td>
       </tr>
       <tr>
-        <td id="main">
+        <td id="main" width="90%">
           <?php displayMain() ?>
         </td>
-        <td id="nav">
+        <td id="nav" width="90%">
           <?php displayNav() ?>
         </td>
       </tr>
