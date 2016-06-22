009eebe91e04b0462a3953b4bee1abaeb677a131
carverh <pcarverb@hotmail.com>
2016-06-21 19:33:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <html>
   <head>
     <title><?php displayTitle() ?></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
   </head>
