da3b2487c6fba69b359f3168ed3e63b0609ca3a5
carverh <pcarverb@hotmail.com>
2016-06-21 20:19:06 -0700

    Update core.php

--- core.php
+++ core.php
@@ -32,7 +32,7 @@
       $file_title = $file->title;
       echo '<article>';
       echo '<h1 class="post-title">';
-      echo '<a class="post-title-link" href="view.php/' . explode($post, '.xml')[1] . '">' . $file_title . '</a>';
+      echo '<a class="post-title-link" href="view.php/' . $post . '">' . $file_title . '</a>';
       echo '</h1>';
       if (strlen($file_text) > $config['home_post_length']) {
         $file_text = str_split($file_text, $config['home_post_length'])[0];
